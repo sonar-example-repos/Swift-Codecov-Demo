3ddc1c66e358002aa122d64ce9408528a45da5cd
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2025-12-10 18:30:34 -0600

    Remove bad creds

--- CodecovDemo/BadCode.swift
+++ CodecovDemo/BadCode.swift
@@ -8,11 +8,6 @@
 import Foundation
 import UIKit
 
-// VULNERABILITY: Hardcoded credentials
-let API_KEY = "sk_live_51234567890abcdefghijklmnop"
-let PASSWORD = "admin123"
-let SECRET_TOKEN = "my-super-secret-token"
-
 class BadCode {
     
     // CODE SMELL: Magic numbers everywhere
